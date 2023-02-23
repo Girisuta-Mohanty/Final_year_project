eb479bc292ce98f721e73d9d87e8cd2384a74613
b219035retr0 <57428197+b219035retr0@users.noreply.github.com>
2023-02-23 15:57:03 +0530

    changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,18 +11,18 @@ import YtVideo from './YtVideo';
 import ImCara from './ImCara';
 import Events from './Events';
 // const uniqueList=[...new Set(Faculty.map((curElem)=>{
-//     return curElem.category;
+//     return curElem.profession;
 // })),"All"];
 const MainPage = () => {
     // const [faculty,setFacultyData]=useState(Faculty);
     // const [MenuList,setMenuList]=useState(uniqueList);
-    // const filterItem=(category)=>{
-    //     if(category==='All'){
+    // const filterItem=(profession)=>{
+    //     if(profession==='All'){
     //      setFacultyData(faculty);
     //      return;
     //     }
     //      const updatedList=Faculty.filter((curElem)=>{
-    //          return curElem.category===category;
+    //          return curElem.profession===profession;
     //      });
     //      setFacultyData(updatedList);
     //  };
